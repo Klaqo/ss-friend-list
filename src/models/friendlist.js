f9a6806fb3038bc89016eb9df81be9fac25a51d4
Klaqo <Sam@Sams-MacBook-Air.local>
2016-04-08 10:53:57 +0100

    posts expire after 3 days

--- src/models/friendlist.js
+++ src/models/friendlist.js
@@ -3,8 +3,9 @@ var mongoose = require('mongoose');
 var friendListSchema = new mongoose.Schema ({
   game_id: Number,
   username: { type: String, maxlength: 30, unique: true},
-  message: { type: String, maxlength: 140}
-}, {timestamps: {createdAt: 'dateAdded'}});
+  message: { type: String, maxlength: 140},
+  createdAt: { type: Date, expires: 259200, default: Date.now}
+});
 
 
 
